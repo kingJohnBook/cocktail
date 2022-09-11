c6d710cd586cbe513a6bf433bcadf9bf75674728
kingjohnbook <johnzkfr@gmail.com>
2022-09-11 18:24:23 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Earum r
+        ecusandae dolor quae, mollitia pariatur quas
+        accusantium tempora possimus itaque exercita
+        tionem voluptatum inventore alias sunt labor
+        um. Modi, ab ullam? Dolores, officiis.
+      </p>
+    </section>
+  )
+}
+
+export default About
